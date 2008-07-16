4d613c3e4d32b323c8a058b81f36313587c56abc
Matt Neuburg <matt@tidbits.com>
2008-07-16 14:53:38 +0000

    starting to clean up rubyfrontier folder

--- #filters/finalFilter.rb
+++
@@ -1,9 +0,0 @@
-def finalFilter(adrPageTable)
-  if adrPageTable[:markdown] || adrPageTable[:smartypants]
-    IO.popen(ENV['TM_SUPPORT_PATH'] + "/bin/SmartyPants.pl", "r+") do |io|
-      io.write adrPageTable[:renderedtext]
-      io.close_write
-      adrPageTable[:renderedtext] = io.read
-    end
-  end
-end
