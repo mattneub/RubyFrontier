b7195f68e135c4a9845a1c4adfbc9e3aaa3463d3
Matt Neuburg <matt@tidbits.com>
2009-07-25 02:02:01 +0000

    found a better way to escape my popen commands without explicitly calling e_sh and without confusing myself

--- RubyFrontier.tmbundle/Support/bin/RubyFrontier/longestJourney/docs/RubyFrontierDocumentation/#filters/finalFilter.rb
+++ RubyFrontier.tmbundle/Support/bin/RubyFrontier/longestJourney/docs/RubyFrontierDocumentation/#filters/finalFilter.rb
@@ -1,9 +1,9 @@
 def finalFilter(adrPageTable)
   # adrPageTable[:renderedtext] = process(adrPageTable[:renderedtext])
   # for example, here's how to get smartypants support:
-  require ENV['TM_SUPPORT_PATH'] + "/lib/escape.rb"
+
   if adrPageTable[:markdown] || adrPageTable[:smartypants]
-    IO.popen(e_sh(ENV['TM_SUPPORT_PATH']) + "/bin/SmartyPants.pl", "r+") do |io|
+    IO.popen(%{"#{ENV['TM_SUPPORT_PATH']}/bin/SmartyPants.pl"}, "r+") do |io|
       io.write adrPageTable[:renderedtext]
       io.close_write
       adrPageTable[:renderedtext] = io.read
--- RubyFrontier.tmbundle/Support/bin/RubyFrontier/longestJourney/newsite/#filters/pageFilter.rb
+++ RubyFrontier.tmbundle/Support/bin/RubyFrontier/longestJourney/newsite/#filters/pageFilter.rb
@@ -1,9 +1,9 @@
 def pageFilter(adrPageTable)
   # adrPageTable[:bodytext] = process(adrPageTable[:bodytext])
   # for example, here's how to get markdown support:
-  require ENV['TM_SUPPORT_PATH'] + "/lib/escape.rb"
+
   if adrPageTable[:markdown]
-    IO.popen(e_sh(ENV['TM_SUPPORT_PATH']) + "/bin/markdown.pl", "r+") do |io|
+    IO.popen(%{"#{ENV['TM_SUPPORT_PATH']}/bin/markdown.pl"}, "r+") do |io|
       io.write adrPageTable[:bodytext]
       io.close_write
       adrPageTable[:bodytext] = io.read
