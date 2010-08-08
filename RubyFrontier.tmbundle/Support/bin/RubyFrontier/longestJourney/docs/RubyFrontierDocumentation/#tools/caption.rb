08db17a6203feba734d0ce830f2dc17de943cf9e
Matt Neuburg <matt@tidbits.com>
2010-08-08 00:14:05 +0000

    updated docs to use more Haml and kramdown features

--- RubyFrontier.tmbundle/Support/bin/RubyFrontier/longestJourney/docs/RubyFrontierDocumentation/#tools/caption.rb
+++ RubyFrontier.tmbundle/Support/bin/RubyFrontier/longestJourney/docs/RubyFrontierDocumentation/#tools/caption.rb
@@ -2,14 +2,15 @@ def caption(img, txt)
   # generate an image-caption pair
   # how wide is the picture?
   width = html.getImageData(img)[:width] + 10
-  %{
-<div><table style="margin-left:auto; margin-right:auto; width:#{width}px"><tr><td class="pic">
-#{imageref(img)}
-</td></tr>
-<tr><td class="caption" markdown="span">
-#{txt}
-</td></tr>
-</table></div>
-  }.gsub("\n", "")
+  s = <<END
+%div
+  %table(style="margin-left:auto; margin-right:auto; width:#{width}px")
+    %tr
+      %td(class="pic")
+        #{imageref(img)}
+    %tr
+      %td(class="caption" markdown="span") #{txt}
+END
+  Haml::Engine.new(s, :ugly => true).render.gsub("\n", "")
 end
 
