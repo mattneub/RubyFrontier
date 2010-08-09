5eb2493ccd009afdc19d06c1f869365ed9f5718d
Matt Neuburg <matt@tidbits.com>
2010-08-09 02:00:15 +0000

    slight update of docs to reflect rejiggered new site model

--- RubyFrontier.tmbundle/Support/bin/RubyFrontier/longestJourney/docs/RubyFrontierDocumentation/#filters/pageFilter.rb
+++ RubyFrontier.tmbundle/Support/bin/RubyFrontier/longestJourney/docs/RubyFrontierDocumentation/#filters/pageFilter.rb
@@ -1,3 +1,6 @@
 def pageFilter(adrPageTable)
-  # adrPageTable[:bodytext] = process(adrPageTable[:bodytext])
+  # support for haml-based template
+  if adrPageTable[:haml] and (t = adrPageTable.fetch2(:template)) and t.kind_of?(Pathname)
+    adrPageTable[:directTemplate] = Haml::Engine.new(File.read(t), :attr_wrapper => '"').render
+  end
 end
