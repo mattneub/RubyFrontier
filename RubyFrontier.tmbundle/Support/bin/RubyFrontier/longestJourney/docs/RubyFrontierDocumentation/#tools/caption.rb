3004c742e4fe2c0f2b57c17d22fa1cec8cd98afe
Matt Neuburg <matt@tidbits.com>
2010-10-17 19:44:44 +0000

    updated docs to talk about cross-refs, some other small changes

--- RubyFrontier.tmbundle/Support/bin/RubyFrontier/longestJourney/docs/RubyFrontierDocumentation/#tools/caption.rb
+++ RubyFrontier.tmbundle/Support/bin/RubyFrontier/longestJourney/docs/RubyFrontierDocumentation/#tools/caption.rb
@@ -1,16 +1,21 @@
-def caption(img, txt)
+def caption(img, txt, xref=nil)
   # generate an image-caption pair
   # how wide is the picture?
   width = html.getImageData(img)[:width] + 10
+  # now also with auto-numbering of figures, just to demonstrate
+  @adrPageTable[:fignum] ||= 0
+  @adrPageTable[:fignum] += 1
+  fignum = "Figure " + @adrPageTable[:fignum].to_s
+  makexref(xref, :fignum => fignum) if xref
   s = <<END
-%div
+%div{:id => xref}
   %table(style="margin-left:auto; margin-right:auto; width:#{width}px")
     %tr
       %td(class="pic")
         #{imageref(img)}
     %tr
-      %td(class="caption" markdown="span") #{txt}
+      %td(class="caption" markdown="span") #{fignum + ": " + txt}
 END
-  Haml::Engine.new(s, :ugly => true).render.gsub("\n", "")
+  Haml::Engine.new(s, :ugly => true).render(Object.new, :xref => xref).gsub("\n", "")
 end
 
